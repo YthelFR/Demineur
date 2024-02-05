bf262765717a7bc1454b3823614ddd2142be7637
JAMBON Yoan <yoanjambon21@macbook-air-de-jambon.home>
2024-02-05 13:39:06 +0100

    Ajout de commentaires pour guider sur la conception du JS

--- js/script.js
+++ js/script.js
@@ -23,7 +23,8 @@ let Demineur = {
 
   parametres: {},
 
-  // Initialisation du tableau de jeu en fonction de la difficulté choisie
+  // Initialisation du tableau de jeu (lignes/colonnes/emplacement des mines)
+  // en fonction de la difficulté choisie
 
   jeu: {
     statut: 0,
@@ -55,7 +56,32 @@ let Demineur = {
 
     // Création des cases du jeu avec une fonction "if" afin de récupérer les informations de
     // difficultés (nombre de lignes/colonnes/mines) et les représenter à l'écran.
+
+    // Définition d'une action au clic pour l'utilisateur afin de pouvoir cliquer sur les
+    // cases du jeu.
+
+    // Empêcher l'utilisateur de pouvoir cliquer en dehors de la zone de jeu afin d'éviter
+    // un bug, pour sécuriser la zone.
+
+    // Ajouter une fonction qui permet de rendre les mines à des emplacements aléatoires
+    // sur toutes les difficultés du jeu afin que chaque partie soit différente.
   },
+
+  // Ajout d'une fonction qui permet à l'utilisateur, en cas de clic-droit sur une case, de
+  // marquer la case afin de marquer les potentielles position des mines. Bien vérifier quelle
+  // case peut-être marquée (au cas où l'utilisateur tente de marquer une case qui est déjà révélée).
+
+  // Rajouter à la fonction une fonction qui permet d'enlever ce marquage précédemment posé.
+
+  // Ajouter une fonction essentielle pour un démineur : révéler toutes les cases dites "vides" ou "0"
+  // qui sont adjacentes les unes aux autres afin de révéler le terrain vide.
+
+  // Ajouter une fonction qui, lorsque l'utilisateur trouve une case "minée", met fin à la partie.
+
+  // Ajouter la fonction qui permet à l'utilisateur de savoir si des mines sont à proximité.
+  // C'est-à-dire d'afficher les numeros "1", "2", "3"...
+
+  // Ajouter les fonctions qui permettent d'afficher la "Victoire" ou la "Défaite" de l'utilisateur.
 
   // Fonction reset du plateau en cas de partie terminée ou qui initialisera les données du programme
 
